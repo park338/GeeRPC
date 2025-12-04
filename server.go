c19bbd1c649055da3ba5955306619b312c1c8b19
Administrator <1248814210@qq.com>
2025-12-04 17:17:05 +0800

    实现服务注册

--- server.go
+++ server.go
@@ -3,12 +3,15 @@ package GeeRPC
 import (
 	"codec/codec"
 	"encoding/json"
-	"fmt"
+	"errors"
+	"go/ast"
 	"io"
 	"log"
 	"net"
 	"reflect"
+	"strings"
 	"sync"
+	"sync/atomic"
 )
 
 const MagicNumber = 0x3bef5c
@@ -24,14 +27,145 @@ var DefaultOption = &Option{
 	CodecType:   codec.GobType,
 }
 
-type Server struct{}
+// 方法的结构体
+type methodType struct {
+	method    reflect.Method
+	ArgType   reflect.Type
+	ReplyType reflect.Type
+	numCalls  uint64
+}
+
+// 方法调用计数
+func (m *methodType) NumCalls() uint64 {
+	return atomic.LoadUint64(&m.numCalls)
+}
+
+// 获取第一个参数实例
+func (m *methodType) NewArgv() reflect.Value {
+	var Argv reflect.Value
+	//判断类型
+	if m.ArgType.Kind() == reflect.Ptr {
+		Argv = reflect.New(m.ArgType.Elem())
+	} else {
+		Argv = reflect.New(m.ReplyType).Elem()
+	}
+	return Argv
+}
+
+// 获取第二个参数实例
+func (m *methodType) newReplyv() reflect.Value {
+	Replyv := reflect.New(m.ReplyType.Elem())
+	switch m.ReplyType.Elem().Kind() {
+	case reflect.Map:
+		Replyv.Elem().Set(reflect.MakeMap(m.ReplyType.Elem()))
+	case reflect.Slice:
+		Replyv.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
+	}
+	return Replyv
+}
+
+// 结构体服务
+type service struct {
+	name   string
+	typ    reflect.Type
+	rcvr   reflect.Value
+	method map[string]*methodType
+}
+
+func newService(rcvr interface{}) *service {
+	s := new(service)
+	s.rcvr = reflect.ValueOf(rcvr)
+	s.name = reflect.Indirect(s.rcvr).Type().Name()
+	s.typ = reflect.TypeOf(rcvr)
+	if !ast.IsExported(s.name) {
+		log.Fatalf("rpc服务：%s 不是一个合法的名字", s.name)
+	}
+	s.registerMethods()
+	return s
+}
+
+// 服务与方法绑定
+func (s *service) registerMethods() {
+	s.method = make(map[string]*methodType)
+	for i := 0; i < s.typ.NumMethod(); i++ {
+		method := s.typ.Method(i)
+		mType := method.Type
+		//方法的合理性判断
+		if mType.NumIn() != 3 || mType.NumOut() != 1 {
+			continue
+		}
+		if mType.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+			continue
+		}
+		argType, replyType := mType.In(1), mType.In(2)
+		if !isExportedOrBuiltinType(argType) || !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+		s.method[method.Name] = &methodType{
+			method:    method,
+			ArgType:   argType,
+			ReplyType: replyType,
+		}
+	}
+}
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
+}
+
+// 调用方法
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+	f := m.method.Func
+	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
+	if errInter := returnValues[0].Interface(); errInter != nil {
+		return errInter.(error)
+	}
+	return nil
+}
+
+type Server struct {
+	serviceMap sync.Map
+}
 
 func NewServer() *Server {
 	return &Server{}
 }
 
+// 注册服务
+func (server *Server) Register(rcvr interface{}) error {
+	s := newService(rcvr)
+	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
+		return errors.New("rpc服务注册出错 " + s.name)
+	}
+	return nil
+}
+
 // 默认服务
 var DefaultServer = NewServer()
+
+// 默认服务注册
+func Register(rcvr interface{}) error { return DefaultServer.Register(rcvr) }
+
+// 服务查找
+func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("rpc服务名传递错误" + serviceMethod)
+		return
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
+	svci, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc服务查找失败" + serviceName)
+		return
+	}
+	svc = svci.(*service)
+	mtype = svc.method[methodName]
+	if mtype != nil {
+		err = errors.New("rpc服务找不到方法" + methodName)
+	}
+	return
+}
 
 func (server *Server) Accept(lis net.Listener) {
 	for {
@@ -77,7 +211,7 @@ func (server *Server) serveCodec(cc codec.Codec) {
 			if req == nil {
 				break
 			}
-			server.sendResopnse(cc, req.h, invalidRequest, sending)
+			server.sendResponse(cc, req.h, invalidRequest, sending)
 		}
 		wg.Add(1)
 		server.handlerRequest(cc, req, sending, wg)
@@ -89,6 +223,8 @@ func (server *Server) serveCodec(cc codec.Codec) {
 type request struct {
 	h            *codec.Header //请求头
 	argv, replyv reflect.Value
+	mtype        *methodType
+	svc          *service
 }
 
 // 读取请求头
@@ -110,18 +246,28 @@ func (server *Server) readRequest(cc codec.Codec) (*request, error) {
 		return nil, err
 	}
 	req := &request{h: h}
-	req.argv = reflect.New(reflect.TypeOf(""))
-	if err = cc.ReadBody(req.argv.Interface()); err != nil {
-		log.Println("rpc server:读取argv错误", err)
+	req.svc, req.mtype, err = server.findService(h.ServiceMethod)
+	if err != nil {
+		return req, err
+	}
+	req.argv = req.mtype.NewArgv()
+	req.replyv = req.mtype.newReplyv()
+
+	argvi := req.argv.Interface()
+	if req.argv.Type().Kind() != reflect.Ptr {
+		argvi = req.argv.Addr().Interface()
+	}
+	if err = cc.ReadBody(argvi); err != nil {
+		log.Println("rpc服务读取请求体错误", err)
+		return req, err
 	}
 	return req, nil
 }
 
 // 发送请求
-func (server *Server) sendResopnse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
+func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
 	sending.Lock()
 	defer func() { sending.Unlock() }()
-	//TODO不知道argv的类型
 	if err := cc.Write(h, body); err != nil {
 		log.Println("rpc server: 响应失败:", err)
 	}
@@ -129,9 +275,12 @@ func (server *Server) sendResopnse(cc codec.Codec, h *codec.Header, body interfa
 
 // 处理请求
 func (server *Server) handlerRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
-	//TODO需要正确的rpc服务获取replyv
 	defer wg.Done()
-	log.Println(req.h, req.argv.Elem())
-	req.replyv = reflect.ValueOf(fmt.Sprintf("geerpc resp %d", req.h.Seq))
-	server.sendResopnse(cc, req.h, req.replyv.Interface(), sending)
+	err := req.svc.call(req.mtype, req.argv, req.replyv)
+	if err != nil {
+		req.h.Error = err.Error()
+		server.sendResponse(cc, req.h, invalidRequest, sending)
+		return
+	}
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
 }
